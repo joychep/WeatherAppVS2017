2291f844cda9c376b4ae2827cf4aafbe4714cecb
joychepkwony <joychepkwony@gmail.com>
2017-09-22 08:01:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather
+This is a xamarin mobile cross platform application that displays the current weather given a particular zip code.
+The weather data is obtained from an api call to openweathermap.org.
---
+++ WeatherApp/WeatherApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WeatherApp.UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+       public MainPage()
+       {
+            InitializeComponent();
+       }
+
+        private async void GetWeatherButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                locationText.Text = weather.Title;
+                tempText.Text = weather.Temperature;
+                windText.Text = weather.Wind;
+                visibilityText.Text = weather.Visibility;
+                humidityText.Text = weather.Humidity;
+                sunriseText.Text = weather.Sunrise;
+                sunsetText.Text = weather.Sunset;
+
+                weatherBtn.Content = "Search Again";
+            }
+        }
+
+    }
+}
